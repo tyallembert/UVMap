4918f5398c425367f55e941814eabfba59b24893
Ty Allembert <tyallembert@gmail.com>
2022-09-23 12:58:17 -0400

    Initial Commit

---
+++ UVMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UVMap
+//
+//  Created by Ty Allembert on 9/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UVMap/UVMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UVMapApp.swift
+//  UVMap
+//
+//  Created by Ty Allembert on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct UVMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
