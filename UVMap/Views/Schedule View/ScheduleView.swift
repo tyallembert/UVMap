ccbf43b60ed76dd4190fca3cdca2278280b56815
Ty Allembert <tyallembert@gmail.com>
2022-09-24 12:18:15 -0400

    Added basic settings and schedule view

---
+++ UVMap/Views/Schedule View/ScheduleView.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleView.swift
+//  UVMap
+//
+//  Created by Ty Allembert on 9/24/22.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScheduleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleView()
+    }
+}
---
+++ UVMap/Views/Settings View/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  UVMap
+//
+//  Created by Ty Allembert on 9/24/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
