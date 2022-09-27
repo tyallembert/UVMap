e877655556f6143cc8514d672ef0a8b03397ae9f
Ty Allembert <tyallembert@gmail.com>
2022-09-27 13:55:06 -0400

    Slight structure changes

---
+++ UVMap/Views/Login View/UsernameView.swift
@@ -0,0 +1,30 @@
+//
+//  UsernameView.swift
+//  UVMap
+//
+//  Created by Ty Allembert on 9/27/22.
+//
+
+import SwiftUI
+
+struct UsernameView: View {
+    @Binding var username: String
+    
+    var body: some View {
+        Text("NETID")
+            .fontWeight(.medium)
+            .offset(x: 8, y: 12)
+            .foregroundColor(Color(red: 41 / 255, green: 97 / 255, blue: 54 / 255))
+        TextField("", text: $username)
+            .padding(7)
+            .background(Color.white)
+            .cornerRadius(100)
+            .shadow(color: .black, radius: 3, x: 1, y: 2)
+    }
+}
+
+//struct UsernameView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        UsernameView(username: $username)
+//    }
+//}
