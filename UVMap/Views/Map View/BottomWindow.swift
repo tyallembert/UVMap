efa8e718221079b21a12f57c1737eb49335ca4fe
Ty Allembert <tyallembert@gmail.com>
2022-10-12 14:54:29 -0400

    map page UI work, added bottom window draggable sheet

--- UVMap/Views/Map View/BottomWindow.swift
+++ UVMap/Views/Map View/BottomWindow.swift
@@ -14,7 +14,7 @@ struct BottomWindow: View {
                 HStack {
                     VStack {
                         HStack {
-                            ClassBuilding()
+//                            ClassBuilding()
                             Spacer()
                         }
                         HStack {
@@ -32,14 +32,36 @@ struct BottomWindow: View {
                 StartButton()
                 TripInformation()
             }
+            .padding()
         }
-        .frame(height: 400)
+        .backgroundBlur(radius: 25, opaque: true)
+        .background(Color.bottomSheetBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 35))
+        .innerShadow(shape: RoundedRectangle(cornerRadius: 35), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1)
+        .overlay{
+            // MARK: bottom sheet seperator
+            Divider()
+                .blendMode(.overlay)
+                .background(Color.bottomSheetBorderTop)
+                .frame(maxHeight: .infinity, alignment: .top)
+                .clipShape(RoundedRectangle(cornerRadius: 35))
+        }
+        .overlay{
+            // MARK: little drag indicator
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.black.opacity(0.3))
+                .frame(width: 48, height: 5)
+                .frame(height: 20)
+                .frame(maxHeight: .infinity, alignment: .top)
+        }
     }
 }
 
 struct BottomWindow_Previews: PreviewProvider {
     static var previews: some View {
         BottomWindow()
+            .background(Color.background)
+            .preferredColorScheme(.dark)
     }
 }
 
