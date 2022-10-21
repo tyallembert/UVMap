042a841ec9aeee0dabfd71a44be578e9ae203cc3
tateosborne <tate0301@gmail.com>
2022-10-21 11:48:07 -0400

    Made more changes to SignUpInnerBoxes to organize subviews

---
+++ UVMap/Views/SignUp View/SignUpButton.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpButton.swift
+//  UVMap
+//
+//  Created by Tate Osborne on 10/21/22.
+//
+
+import SwiftUI
+
+struct SignUpButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpButton()
+    }
+}
