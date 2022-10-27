5f9bb510a31b99b71ab86fb140abaae2c542afb4
tateosborne <tate0301@gmail.com>
2022-10-27 10:35:19 -0400

    final view files added

---
+++ UVMap/Views/SignUp View/SignUpPasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpPasswordView.swift
+//  UVMap
+//
+//  Created by Tate Osborne on 10/27/22.
+//
+
+import SwiftUI
+
+struct SignUpPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpPasswordView()
+    }
+}
---
+++ UVMap/Views/SignUp View/SignUpRetypePasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpRetypePasswordView.swift
+//  UVMap
+//
+//  Created by Tate Osborne on 10/27/22.
+//
+
+import SwiftUI
+
+struct SignUpRetypePasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpRetypePasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpRetypePasswordView()
+    }
+}
