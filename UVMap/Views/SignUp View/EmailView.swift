b692d4c240ce95c6a6d8b28cf608456e4f2e4855
tateosborne <tate0301@gmail.com>
2022-10-27 10:24:24 -0400

    created email view for sign up

---
+++ UVMap/Views/SignUp View/EmailView.swift
@@ -0,0 +1,20 @@
+//
+//  EmailView.swift
+//  UVMap
+//
+//  Created by Tate Osborne on 10/27/22.
+//
+
+import SwiftUI
+
+struct EmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmailView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmailView()
+    }
+}
