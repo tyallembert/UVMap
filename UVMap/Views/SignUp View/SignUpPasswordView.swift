e1eae7a440198fc1fd3a0532339ecd358b53f31b
tateosborne <tate0301@gmail.com>
2022-10-27 10:38:51 -0400

    view structures for sign up view done - need to debug/edit references in backend

--- UVMap/Views/SignUp View/SignUpPasswordView.swift
+++ UVMap/Views/SignUp View/SignUpPasswordView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct SignUpPasswordView: View {
+    @EnvironmentObject var sessionsManager: SessionManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Password")
+            .fontWeight(.medium)
+            .offset(x: 8, y: 12)
+            .foregroundColor(Color(red: 41 / 255, green: 97 / 255, blue: 54 / 255))
+        TextField("", text: $sessionManager.signUpPassword)
+            .foregroundColor(Color.black)
+            .padding(7)
+            .background(Color.white)
+            .cornerRadius(100)
+            .shadow(color: .black, radius: 3, x: 1, y: 2)
+            .autocapitalization(.none)
     }
 }
 
 struct SignUpPasswordView_Previews: PreviewProvider {
     static var previews: some View {
-        SignUpPasswordView()
+        SignUpPasswordView(signUpPassword: $signUpPassword)
     }
 }
