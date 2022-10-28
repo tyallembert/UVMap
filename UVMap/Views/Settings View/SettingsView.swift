817c198f70562ad451a4a474c8e17757abfff81f
Emmett <ewramm@gmail.com>
2022-10-28 10:40:52 -0400

    Possible manager. might save to file

--- UVMap/Views/Settings View/SettingsView.swift
+++ UVMap/Views/Settings View/SettingsView.swift
@@ -9,40 +9,42 @@ import SwiftUI
 
 struct SettingsView: View {
     @EnvironmentObject var databaseManager: DatabaseManager
-    
     var body: some View {
-        VStack{
-            
-            TitleBar()
-                .padding(.top)
-                .edgesIgnoringSafeArea(.top)
-            
-            VStack(alignment: .leading, spacing: 15) {
+        ZStack{
+            VStack{
+                TitleBar()
+                    .padding(.top)
+                    .edgesIgnoringSafeArea(.top)
                 
-                VStack(alignment: .leading, spacing: 10){
-                    Text("How early do you like to arrive to class?")
-                        .font(.title3)
+                VStack(alignment: .leading, spacing: 15) {
                     
-                    ArriveShell()
-                }
-                VStack(alignment: .leading, spacing: 10){
-                    Text("Prioritize Schedule?")
-                        .font(.title3)
-                    PrioritizeSchedule()
-                }
-                VStack(alignment: .leading, spacing: 10){
-                    Text("Theme")
-                        .font(.title3)
-                    ThemePicker()
-                }
-                Spacer()
-            }.padding()
-        }.background(Color("BG2"))
+                    VStack(alignment: .leading, spacing: 10){
+                        Text("How early do you like to arrive to class?")
+                            .font(.title3)
+                        
+                        ArriveShell()
+                    }
+                    VStack(alignment: .leading, spacing: 10){
+                        Text("Prioritize Schedule?")
+                            .font(.title3)
+                        PrioritizeSchedule()
+                    }
+                    VStack(alignment: .leading, spacing: 10){
+                        Text("Theme")
+                            .font(.title3)
+                        ThemePicker()
+                    }
+                    Spacer()
+                    
+                }.padding()
+            }
+        }
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
+            .environment(\.colorScheme, .dark)
     }
 }
