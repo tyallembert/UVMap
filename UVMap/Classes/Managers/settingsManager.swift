f2191d013ce6acea0a25873cfb3b8d4cfae8cc3f
Emmett <ewramm@gmail.com>
2022-10-28 11:54:34 -0400

    Working on manager

--- UVMap/Classes/Managers/settingsManager.swift
+++ UVMap/Classes/Managers/settingsManager.swift
@@ -12,6 +12,33 @@ class SettingsManager: ObservableObject{
     @Published var prioritizeSchedule: Bool
     @Published var howEarly: Int
     
+    //===Read from Json file===
+    func retrieveClasssesLocally(fileName: String) -> [SingleClass]{
+        let data: Data
+        
+        guard let filePath = Bundle.main.url(forResource: fileName, withExtension: "json")
+        else{
+            print("Couldn't find \(fileName) in main bundle.")
+            return []
+        }
+        
+        do {
+            data = try Data(contentsOf: filePath)
+        } catch {
+            print("Couldn't load \(fileName) from main bundle:\n\(error)")
+            return []
+        }
+
+        do {
+            let decoder = JSONDecoder()
+//            return try decoder.decode(T.self, from: data)
+            return try decoder.decode([SingleClass].self, from: data)
+        } catch {
+            print("File is Empty: \(error)")
+            return []
+        }
+    }
+    
     init(){
         
     }
