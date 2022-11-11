d10fc8e2a78ec921513a2130de8e956eeb67d9bc
Jack McGowan <jackmcgowan12@gmail.com>
2022-11-10 23:19:27 -0500

    fix bug allowing for results to display with end route button and no search bar

--- UVMap/Views/Map View/BuildingSearchResults.swift
+++ UVMap/Views/Map View/BuildingSearchResults.swift
@@ -15,37 +15,43 @@ struct BuildingSearchResults: View {
     
     var body: some View {
         VStack {
-            SearchBar()
-                .environmentObject(mapManager)
-            ScrollView {
-                LazyVStack {
-                    ForEach(mapManager.activeBuildingsFromSearch, id: \.self.id) { building in
-                        OneBuildingResult()
-                            .environmentObject(building)
-                            .environmentObject(mapManager)
-                            .onTapGesture {
-                                mapManager.setActiveBuilding(building: building)
-                                withAnimation{
-                                    mapManager.searchActive = false
-                                    mapManager.searchText = ""
-                                    hideKeyboard()
+            if mapManager.routes.isEmpty {
+                SearchBar()
+                    .environmentObject(mapManager)
+                ScrollView {
+                    LazyVStack {
+                        ForEach(mapManager.activeBuildingsFromSearch, id: \.self.id) { building in
+                            OneBuildingResult()
+                                .environmentObject(building)
+                                .environmentObject(mapManager)
+                                .onTapGesture {
+                                    mapManager.setActiveBuilding(building: building)
+                                    withAnimation{
+                                        mapManager.searchActive = false
+                                        mapManager.searchText = ""
+                                        hideKeyboard()
+                                    }
                                 }
-                            }
-                        Divider()
+                            Divider()
+                        }
                     }
                 }
+                .frame(height: mapManager.searchActive ? 250: 0)
+                .backgroundBlur(radius: 15, opaque: true)
+                .background(Color.white.opacity(0.4))
+                .innerShadow(shape: RoundedRectangle(cornerRadius: 10), color: Color.innerShadow, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 2, blendMode: .overlay, opacity: 0.7)
+                .cornerRadius(10)
+                .offset(y: -5)
+                .shadow(color: .black.opacity(0.3),radius: 7)
+                .onAppear{
+                    mapManager.filterBuildings()
+                }
+                .padding(5)
             }
-            .frame(height: mapManager.searchActive ? 250: 0)
-            .backgroundBlur(radius: 15, opaque: true)
-            .background(Color.white.opacity(0.4))
-            .innerShadow(shape: RoundedRectangle(cornerRadius: 10), color: Color.innerShadow, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 2, blendMode: .overlay, opacity: 0.7)
-            .cornerRadius(10)
-            .offset(y: -5)
-            .shadow(color: .black.opacity(0.3),radius: 7)
-            .onAppear{
-                mapManager.filterBuildings()
+            else {
+                EndRouteButton()
+                    .padding()
             }
-            .padding(5)
         }
     }
 }
--- UVMap/Views/Map View/SearchBar.swift
+++ UVMap/Views/Map View/SearchBar.swift
@@ -13,42 +13,37 @@ struct SearchBar: View {
     
     var body: some View {
         HStack {
-            if mapManager.routes.isEmpty {
-                HStack{
-                    Image(systemName: "magnifyingglass")
-                    TextField("Search for a building", text: $mapManager.searchText)
-                        .onChange(of: mapManager.searchText) {_ in
-                            mapManager.filterBuildings()
-                        }
-                        .onTapGesture {
-                            withAnimation{
-                                mapManager.searchActive = true
-                            }
-                        }
-                        .disableAutocorrection(true)
-                }
-                
-                .padding(7)
-                .backgroundBlur(radius: 25, opaque: true)
-                .background(Color.bottomSheetBackground)
-                .innerShadow(shape: RoundedRectangle(cornerRadius: 100), color: Color.innerShadow, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 0.7)
-                .cornerRadius(100)
-                .shadow(radius: 5)
-                if mapManager.searchActive {
-                    Button{
-                        withAnimation{
-                            mapManager.searchActive = false
-                            hideKeyboard()
-                        }
-                    }label: {
-                        Text("Done")
-                            .foregroundColor(Color.textGreenWhite)
+            HStack{
+                Image(systemName: "magnifyingglass")
+                TextField("Search for a building", text: $mapManager.searchText)
+                    .onChange(of: mapManager.searchText) {_ in
+                        mapManager.filterBuildings()
                     }
-                }
-            } else {
-                EndRouteButton()
+                    .onTapGesture {
+                        withAnimation{
+                            mapManager.searchActive = true
+                        }
+                    }
+                    .disableAutocorrection(true)
             }
             
+            .padding(7)
+            .backgroundBlur(radius: 25, opaque: true)
+            .background(Color.bottomSheetBackground)
+            .innerShadow(shape: RoundedRectangle(cornerRadius: 100), color: Color.innerShadow, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 0.7)
+            .cornerRadius(100)
+            .shadow(radius: 5)
+            if mapManager.searchActive {
+                Button{
+                    withAnimation{
+                        mapManager.searchActive = false
+                        hideKeyboard()
+                    }
+                }label: {
+                    Text("Done")
+                        .foregroundColor(Color.textGreenWhite)
+                }
+            }
         }
         .padding()
     }
