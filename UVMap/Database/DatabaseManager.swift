089b639f2a719842ad6971e48ff82aa10eab1b4c
Jack McGowan <58006559+mcgows@users.noreply.github.com>
2022-11-15 16:32:28 -0500

    Add support for building abbreviations (#55)
    * Add new buildings to application
    
    * Add support for building abbreviations
    
    * Remove deprecated information from buildings

--- UVMap/Database/DatabaseManager.swift
+++ UVMap/Database/DatabaseManager.swift
@@ -42,7 +42,8 @@ class DatabaseManager: ObservableObject{
         for building in newBuildings {
             let id = String(building.id)
             let name = building.title
-            let address = building.address
+            let abbreviation = building.abbreviation
+//            let address = building.address
             let latitude = building.latitude
             let longitude = building.longitude
             var coordinate = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
@@ -52,56 +53,56 @@ class DatabaseManager: ObservableObject{
                 }
             }
             
-            let aBuilding = Building(id: id, name: name, address: address, coordinate: coordinate)
+            let aBuilding = Building(id: id, name: name, abbreviation: abbreviation, coordinate: coordinate)
             self.buildings.append(aBuilding)
         }
     }
     
-    func queryBuildings(){
-        self.buildings.removeAll()
-        let ref = fireStoreDB.collection("Buildings")
-        ref.getDocuments{ snapshot, error in
-            guard error == nil else {
-                print(error!.localizedDescription)
-                return
-            }
-            if let snapshot = snapshot {
-                for document in snapshot.documents {
-                    let data = document.data()
-                    //once the building class is made, make each building
-                    let id = document.documentID
-                    let name = data["name"] as? String ?? ""
-                    let address = data["address"] as? String ?? ""
-                    
-                    var coordinate = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
-                    if let latitude = Double(data["latitude"] as! Substring){
-                        if let longitude = Double(data["longitude"] as! Substring){
-                            coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                        }else{
-                            let _ = print("longitude not set")
-                        }
-                    }else{
-                        let _ = print("latitude not set")
-                    }
-//                    as? Double ?? 0.0
-//                    let longitude = Double(data["longitude"]) as? Double ?? 0.0
-                    
-                    let aBuilding = Building(id: id, name: name, address: address, coordinate: coordinate)
-                    self.buildings.append(aBuilding)
-                }
-            }
-        }
-    }
+//    func queryBuildings(){
+//        self.buildings.removeAll()
+//        let ref = fireStoreDB.collection("Buildings")
+//        ref.getDocuments{ snapshot, error in
+//            guard error == nil else {
+//                print(error!.localizedDescription)
+//                return
+//            }
+//            if let snapshot = snapshot {
+//                for document in snapshot.documents {
+//                    let data = document.data()
+//                    //once the building class is made, make each building
+//                    let id = document.documentID
+//                    let name = data["name"] as? String ?? ""
+//                    let address = data["address"] as? String ?? ""
+//                    
+//                    var coordinate = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+//                    if let latitude = Double(data["latitude"] as! Substring){
+//                        if let longitude = Double(data["longitude"] as! Substring){
+//                            coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//                        }else{
+//                            let _ = print("longitude not set")
+//                        }
+//                    }else{
+//                        let _ = print("latitude not set")
+//                    }
+////                    as? Double ?? 0.0
+////                    let longitude = Double(data["longitude"]) as? Double ?? 0.0
+//                    
+//                    let aBuilding = Building(id: id, name: name, address: address, coordinate: coordinate)
+//                    self.buildings.append(aBuilding)
+//                }
+//            }
+//        }
+//    }
     //This function is for writing a new building to the database
     func writeBuildings(){
         let newBuildings: [ReadInBuilding] = readBuildingsFromJSON("all_buildings.json")
-        self.queryBuildings()
+//        self.queryBuildings()
         
         for building in newBuildings {
             var buildingInDatabase = false
             
             let name = building.title
-            let address = building.address
+//            let address = building.address
             let latitude = building.latitude
             let longitude = building.longitude
             //checks latitude and longitude to see if that coordinate
@@ -119,7 +120,7 @@ class DatabaseManager: ObservableObject{
             //add to database if it isn't already in there
             if !buildingInDatabase {
                 let ref = fireStoreDB.collection("Buildings").document()
-                ref.setData(["name": name, "address": address, "latitude": latitude, "longitude": longitude]) {error in
+                ref.setData(["name": name, "latitude": latitude, "longitude": longitude]) {error in
                     if let error = error {
                         print(error.localizedDescription)
                     }
