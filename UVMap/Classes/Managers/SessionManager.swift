f659d53f2f1e53c67a3440ff105c591ebac69ce4
Jack McGowan <jackmcgowan12@gmail.com>
2022-11-18 23:43:37 -0500

    Add sign out function to session manager

--- UVMap/Classes/Managers/SessionManager.swift
+++ UVMap/Classes/Managers/SessionManager.swift
@@ -40,6 +40,16 @@ class SessionManager: ObservableObject {
             }
         }
     }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+        }
+        catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+        
+    }
  
     func signIn(database: DatabaseManager){
     usernameInFocus = false
